4f2022c36c66a30ad64548ea7f8aa1c0f7db0a13
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-11-18 19:18:38 +0000

    TextView working out

--- app/browse/browse-view-model.ts
+++ app/browse/browse-view-model.ts
@@ -1,19 +1,19 @@
 import { Observable } from "tns-core-modules/data/observable";
 import * as dialogs from "tns-core-modules/ui/dialogs";
 import { Page, View } from "tns-core-modules/ui/page";
-import { TextField } from "tns-core-modules/ui/text-field";
+import { TextView } from "tns-core-modules/ui/text-view";
 import { FlexboxLayout } from "tns-core-modules/ui/layouts/flexbox-layout/flexbox-layout";
 import * as Clipboard from "nativescript-clipboard";
 
 export class BrowseViewModel extends Observable {
-    private textField?: TextField;
-    private _textFieldValue: string = "";
+    private textView?: TextView;
 
-    get textFieldValue(): string { return this._textFieldValue; }
-    set textFieldValue(value: string) {
-        if (this._textFieldValue === value) return;
-        this._textFieldValue = value;
-        this.notifyPropertyChange('textFieldValue', value);
+    private _textViewValue: string = "";
+    get textViewValue(): string { return this._textViewValue; }
+    set textViewValue(value: string) {
+        if (this._textViewValue === value) return;
+        this._textViewValue = value;
+        this.notifyPropertyChange('textViewValue', value);
     }
 
     constructor() {
@@ -21,7 +21,7 @@ export class BrowseViewModel extends Observable {
     }
 
     clear() {
-        this.textFieldValue = "";
+        this.textViewValue = "";
     }
 
     navigatingTo(args) {
@@ -33,7 +33,7 @@ export class BrowseViewModel extends Observable {
     }
 
     firstTfLoaded(args) {
-        const firstTextfield: TextField = <TextField>args.object;
+        const firstTextfield: TextView = <TextView>args.object;
         // setTimeout(() => {
         //     firstTextfield.focus(); // Shows the soft input method, ususally a soft keyboard.
         // }, 100);
@@ -49,32 +49,40 @@ export class BrowseViewModel extends Observable {
             console.error(e);
         });
     }
+
+    onTextViewLoaded(args){
+        const textView: TextView = <TextView>args.object;
+        textView.style.fontFamily = "Courier New";
+        textView.style.fontSize = 16;
+
+        textView.on("textChange", (argstv) => {
+            console.dir(argstv);
+        });
+    }
     
     onReturnPress(args) {
         // returnPress event will be triggered when user submits a value
-        const textField: TextField = <TextField>args.object;
+        const textView: TextView = <TextView>args.object;
         // Gets or sets the placeholder text.
-        console.log(textField.hint);
+        console.log(textView.hint);
         // Gets or sets the input text.
-        console.log(textField.text);
-        // Gets or sets the secure option (e.g. for passwords).
-        console.log(textField.secure);
+        console.log(textView.text);
     
         // Gets or sets the soft keyboard type. Options: "datetime" | "phone" | "number" | "url" | "email"
-        console.log(textField.keyboardType);
+        console.log(textView.keyboardType);
         // Gets or sets the soft keyboard return key flavor. Options: "done" | "next" | "go" | "search" | "send"
-        console.log(textField.returnKeyType);
+        console.log(textView.returnKeyType);
         // Gets or sets the autocapitalization type. Options: "none" | "words" | "sentences" | "allcharacters"
-        console.log(textField.autocapitalizationType);
+        console.log(textView.autocapitalizationType);
     
         // Gets or sets a value indicating when the text property will be updated.
-        console.log(textField.updateTextTrigger);
+        console.log(textView.updateTextTrigger);
         // Gets or sets whether the instance is editable.
-        console.log(textField.editable);
+        console.log(textView.editable);
         // Enables or disables autocorrection.
-        console.log(textField.autocorrect);
+        console.log(textView.autocorrect);
         // Limits input to a certain number of characters.
-        console.log(textField.maxLength);
+        console.log(textView.maxLength);
     
         // setTimeout(() => {
         //     textField.dismissSoftInput(); // Hides the soft input method, ususally a soft keyboard.
@@ -88,7 +96,7 @@ export class BrowseViewModel extends Observable {
     
     onBlur(args) {
         // blur event will be triggered when the user leaves the TextField
-        const textField: TextField = <TextField>args.object;
+        const textView: TextView = <TextView>args.object;
         // textField.dismissSoftInput();
         console.log("onBlur event");
     }
