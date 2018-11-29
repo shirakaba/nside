7a3dac7fc4df366bf09c3357285a124852342e8d
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-11-29 19:40:12 +0000

    Moved SyntaxView

--- app/components/syntax-view/SyntaxView.ts
+++ app/components/syntax-view/SyntaxView.ts
@@ -1,7 +1,13 @@
 import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout/stack-layout";
 import { SyntaxHighlighter, CodeAttributedStringWrapper } from "nativescript-syntax-highlighter";
 
-export function onLoad(args) {
+/* 
+https://docs.nativescript.org/plugins/ui-plugin-custom 
+https://moduscreate.com/blog/custom-components-in-nativescript/
+https://docs.nativescript.org/core-concepts/navigation
+https://docs.nativescript.org/ui/ns-ui-widgets/placeholder
+*/
+export function creatingView(args) {
     const container: StackLayout = args.object;
     console.log("container", container);
     const uiView: UIView = container.ios.view;
@@ -26,5 +32,7 @@ export function onLoad(args) {
     textView.autocapitalizationType = UITextAutocapitalizationType.None;
     textView.textColor = UIColor.alloc().initWithWhiteAlpha(0.8, 1.0);
 
-    uiView.addSubview(textView);
+    /* https://docs.nativescript.org/ui/ns-ui-widgets/placeholder */
+    // uiView.addSubview(textView);
+    args.view = textView;
 }
