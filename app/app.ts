b65e76765329ee522c8d3421c7966bcf64386d7f
Jamie Birch work laptop <jbizzle5@gmail.com>
2018-12-01 21:14:29 +0000

    UINode exposed

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,33 @@ purpose of the file is to pass control to the app’s first module.
 */
 import * as app from "tns-core-modules/application";
 (global as any).app = app;
+(global as any).UINode = function UINode(child) {
+	this.child = child;
+	this.name = child.typeName;
+	this.children = [];
+
+	(child.eachChildView ? child.eachChildView : child.eachChild).bind(child)((subChild) => {
+		this.children.push(new UINode(subChild));
+		return true;
+	});
+
+	this.toString = function(tabDepth){
+		if(typeof tabDepth === "undefined") tabDepth = 0;
+		const tab = new Array(tabDepth).fill("  ").join('');
+
+		let buffer = tab + "<" + this.name;
+		buffer += (this.child.id ? (' id="' + this.child.id + '"') : "");
+		
+		if(this.children.length === 0){
+			buffer += "/>\n"
+		} else {
+			buffer += ">\n";
+			buffer += this.children.map((child) => child.toString(tabDepth + 1)).join("");
+			buffer += tab + "</" + this.name + ">\n"
+		}
+		return buffer;
+	}
+};
 app.run({ moduleName: "app-root" });
 
 /*
