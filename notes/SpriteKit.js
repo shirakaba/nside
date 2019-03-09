0121bce8b38ab0adb0d81fb367a256bc00b77657
Jamie Birch work laptop <jbizzle5@gmail.com>
2019-03-09 13:01:30 +0000

    More mathy approach

--- notes/SpriteKit.js
+++ notes/SpriteKit.js
@@ -1,3 +1,6 @@
+// Ray Wenderlich: https://www.raywenderlich.com/906-scenekit-tutorial-with-swift-part-2-nodes
+// For 3D experiments, see: https://www.weheartswift.com/introduction-scenekit-part-1/
+
 function getUIViewController(uiresponder){
     for(let responder = uiresponder; responder !== null && typeof responder !== "undefined"; responder = responder.nextResponder){
         if(responder instanceof UIViewController) return responder;
@@ -43,16 +46,45 @@ const BattlefieldScene = SKScene.extend(
         },
 
         update: function(currentTime){
-            // const deltaTime = currentTime - (this.lastUpdateTime || ( currentTime - 60));
+            /* Somehow not working: https://stackoverflow.com/questions/33818362/is-there-a-way-to-read-get-fps-in-spritekit-swift */
+            // const deltaTime = currentTime - (this.lastUpdateTime ? this.lastUpdateTime : currentTime - 0.06);
             // const currentFPS = 1 / deltaTime;
             // this.lastUpdateTime = currentTime;
 
+            const idealDeltaTime = 60;
+            const idealFPS = 0.0166666;
+
+            const heroBaseSpeed = 5;
+            const heroSpeed = heroBaseSpeed * (1000 / idealDeltaTime);
+            const maxHeroAdvance = heroSpeed * idealDeltaTime;
+
+            const villainBaseSpeed = 3;
+            const villainSpeed = villainBaseSpeed * (1000 / idealDeltaTime);
+            const maxVillainAdvance = villainSpeed * idealDeltaTime;
+
             const vPos = this.villain.position;
             const hPos = this.hero.position;
-            // Close the gap with the hero within one second
+            /* Close the gap with the hero within one second */
+            // this.villain.position = CGPointMake(
+            //     vPos.x - ((vPos.x - hPos.x) * idealFPS),
+            //     vPos.y - ((vPos.y - hPos.y) * idealFPS)
+            // );
+
             this.villain.position = CGPointMake(
-                vPos.x - ((vPos.x - hPos.x) * 0.01666),
-                vPos.y - ((vPos.y - hPos.y) * 0.01666)
+                Math.max(
+                    0,
+                    Math.min(
+                        vPos.x - ((vPos.x - hPos.x) * maxVillainAdvance),
+                        hPos.x
+                    )
+                ),
+                Math.max(
+                    0,
+                    Math.min(
+                        vPos.y - ((vPos.y - hPos.y) * maxVillainAdvance),
+                        hPos.y
+                    )
+                )
             );
         },
 
